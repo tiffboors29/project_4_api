54350a761e9fa35f43ddf59afc4deb3eddcb6b9f
tiffboors29 <tiffanyboorsma@gmail.com>
2015-09-06 18:07:33 -0400

    Add create and update for beers controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -10,4 +10,32 @@ class BeersController < ApplicationController
     render json: Beer.find(params[:id])
   end
 
+  def create
+    beer = Beer.new(beer_params)
+    if beer.save
+      render json: beer
+    else
+      render json: beer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    beer = Beer.find(params[:id])
+    if beer.update!(vote_params)
+      render json: beer
+    else
+      render json: beer.errors, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def beer_params
+    params.require(:beer).permit(:title, :votes, :brewery, :image, :type)
+  end
+
+  def vote_params
+    params.require(:beer).permit(:votes)
+  end
+
 end
