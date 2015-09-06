be3f1ec8af7175d5fd98c55b0a587bd72c69d2a0
tiffboors29 <tiffanyboorsma@gmail.com>
2015-09-06 18:04:00 -0400

    Add before_action authorize for state and beer controllers

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,42 +1,13 @@
 class BeersController < ApplicationController
 
+  before_action :authorize, only: [:index, :show, :create, :update]
+
   def index
-    # index the top 10 voted beers for the all states
     render json: Beer.all
   end
 
   def show
-    # show the top 10 voted beers for the chosen state
-    render json: Beer.find()
-  end
-
-  def create
-    # when beer receives first vote, add it to beer table
-    voted_beer = Beer.new(beer_params)
-    if voted_beer.save
-      render json: voted_beer
-    else
-      render json: voted_beer.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    # update votes if beer in table receives another vote
-    voted_beer = Beer.find(params[:id])
-    if voted_beer.update!(vote_params)
-      render json: voted_beer
-    else
-      render json: voted_beer.errors, status: :unprocessable_entity
-    end
-  end
-
-private
-  def beer_params
-    params.require(:voted_beer).permit(:title, :brewery, :type, :image, :votes)
-  end
-
-  def vote_params
-    params.require(params.require(:voted_beer).permit(:votes))
+    render json: Beer.find(params[:id])
   end
 
 end
