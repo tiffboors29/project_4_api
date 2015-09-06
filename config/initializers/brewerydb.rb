766d3830c1e9d3395949a35c2c6f7fc0326ea8d7
tiffboors29 <tiffanyboorsma@gmail.com>
2015-09-06 19:05:16 -0400

    Config global brewerydb variable

--- config/initializers/brewerydb.rb
+++ config/initializers/brewerydb.rb
@@ -1,3 +1,7 @@
-BreweryDb.configure do |config|
-  config.apikey = ENV['API_KEY']
+# BreweryDb.configure do |config|
+#   config.apikey = ENV['API_KEY']
+# end
+
+$brewery_db = BreweryDB::Client.new do |config|
+  config.api_key = ENV['API_KEY']
 end
