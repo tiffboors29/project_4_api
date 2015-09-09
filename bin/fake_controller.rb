ddb8e063c50087a22651a2812db5fabac522a188
tiffboors29 <tiffanyboorsma@gmail.com>
2015-09-09 14:44:06 -0400

    Configure query for displaying individual beer info and getting brewery info

--- bin/fake_controller.rb
+++ bin/fake_controller.rb
@@ -4,59 +4,59 @@ end
 
 #--Search for breweries in a state---------------
 #--Creates Hash with k:breweryId & v:Name--------
-# state_breweries = brewery_db.locations.all(region: ARGV[0])
+state_breweries = brewery_db.locations.all(region: ARGV[0])
 
-# state_hash = {}
+state_hash = {}
 
-# state_breweries.each do |b|
-#   content = {}
-#   content['name'] = b.brewery.name
-#   content['id'] = b.breweryId
-#   content['website'] = b.brewery.website
-#   state_hash[b.breweryId] = content
-# end
+state_breweries.each do |b|
+  content = {}
+  content['name'] = b.brewery.name
+  content['id'] = b.breweryId
+  content['website'] = b.brewery.website
+  state_hash[b.breweryId] = content
+end
 
 #--Search for breweries in a city---------------
 #--Creates Hash with k:breweryId & v:Name--------
-# city_breweries = brewery_db.locations.all(locality: ARGV[0])
+city_breweries = brewery_db.locations.all(locality: ARGV[0])
 
-# city_hash = {}
+city_hash = {}
 
-# city_breweries.each do |b|
-#   content = {}
-#   content['name'] = b.brewery.name
-#   content['id'] = b.breweryId
-#   content['website'] = b.brewery.website
-#   city_hash[b.breweryId] = content
-# end
+city_breweries.each do |b|
+  content = {}
+  content['name'] = b.brewery.name
+  content['id'] = b.breweryId
+  content['website'] = b.brewery.website
+  city_hash[b.breweryId] = content
+end
 
 #--Search for all beers in a state---------------
 #--Creates Hash with --------
-# state_breweries = brewery_db.locations.all(region: ARGV[0])
+state_breweries = brewery_db.locations.all(region: ARGV[0])
 
-# state_arr = []
+state_arr = []
 
-# # adds each brewery id to state_arr
-# state_breweries.each do |b|
-#   state_arr << b.breweryId
-# end
+# adds each brewery id to state_arr
+state_breweries.each do |b|
+  state_arr << b.breweryId
+end
 
-# beer_hash = {}
+beer_hash = {}
 
-# state_arr.each do |b|
-#   beers = brewery_db.brewery(b).beers
-#   beers.each do |i|
-#     content = {}
-#     content['name'] = i.name_display
-#     content['id'] = i.id
-#     content['abv'] = i.abv
-#     content['breweryId'] = b
-#     content['ibu'] = i.ibu
-#     content['isOrganic'] = i.isOrganic
-#     content['description'] = i.description
-#     beer_hash[i.id] = content
-#   end
-# end
+state_arr.each do |b|
+  beers = brewery_db.brewery(b).beers
+  beers.each do |i|
+    content = {}
+    content['name'] = i.name_display
+    content['id'] = i.id
+    content['abv'] = i.abv
+    content['breweryId'] = b
+    content['ibu'] = i.ibu
+    content['isOrganic'] = i.isOrganic
+    content['description'] = i.description
+    beer_hash[i.id] = content
+  end
+end
 
 #--------------------------------------------
 # create voted beer
@@ -80,7 +80,7 @@ content['brewery_id'] = data['breweries'].first['id']
 content['brewery_name'] = data['breweries'].first['name']
 content['brewery_website'] = data['breweries'].first['website']
 content['brewery_image'] = data['breweries'].first['images']['medium']
-
+content['state'] = data['breweries'].first['locations'].first['region']
 beer_hash['beer_id'] = content
 
 p beer_hash
