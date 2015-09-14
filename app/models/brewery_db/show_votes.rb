c12f68840a2e5afecc0ca97a7f89b270ba497afe
tiffboors29 <tiffanyboorsma@gmail.com>
2015-09-14 13:32:35 -0400

    Add brewery_db models for showing beer and showing beer with votes

---
+++ app/models/brewery_db/show_votes.rb
@@ -0,0 +1,38 @@
+class BreweryDb
+  class ShowVotes
+
+    def initialize(beerId, num_votes)
+      @beer_id = beerId
+      @votes = num_votes
+      @response = request
+      @parsed_data = parse
+    end
+
+    def request
+      HTTParty.get('http://api.brewerydb.com/v2/beer/' + @beer_id + '?withBreweries=Y&key=' + ENV['API_KEY'] + '&format=json')
+    end
+
+    def parse
+      @response.parsed_response['data']
+    end
+
+    def results
+      data = @parsed_data
+      hash = {}
+      content = {}
+      content['beer_id'] = data['id']
+      content['name'] = data['nameDisplay']
+      content['abv'] = data['abv']
+      content['ibu'] = data['ibu']
+      content['isOrganic'] = data['isOrganic']
+      content['description'] = data['style']['description']
+      content['brewery_id'] = data['breweries'].first['id']
+      content['brewery_name'] = data['breweries'].first['name']
+      content['brewery_website'] = data['breweries'].first['website']
+      content['state'] = data['breweries'].first['locations'].first['region']
+      content['votes'] = @votes
+      hash['beer_id'] = content
+      return hash
+    end
+  end
+end
